84169c16b7adb788d9fd501fb1e66530596ceb74
Jesus <blazikenxdeftem@gmail.com>
2022-12-18 22:28:15 -0800

    final commitssds

--- quizz/urls.py
+++ quizz/urls.py
@@ -11,15 +11,14 @@ urlpatterns = [
     path('quiz/procesando-resultados/<slug:quiz_slug>/', views.calificador, name='calificador'),
     
     path('quiz/crear-quiz/de/<usuario>/', views.createQuiz, name='create_quiz'),
+    path('quiz/editar-quiz/<usuario>/<slug:quiz_slug>/', views.editQuiz, name='edit_quiz'),
     path('quiz/crear-preguntas/<usuario>/<slug:quiz_slug>/', views.createQuestions, name='create_questions'),
     path('quiz/crear-opciones/<usuario>/<slug:quiz_slug>/<int:pregunta_id>/', views.createAnswers, name='create_answers'),
     path('quiz/editar-pregunta/<usuario>/<slug:quiz_slug>/<int:pregunta_id>/', views.editPregunta, name='edit_pregunta'),
     path('quiz/eliminar-preguntas/<usuario>/<slug:quiz_slug>/<int:pregunta_id>/', views.deleteQuestions, name='delete_questions'),
     path('quiz/eliminar-opciones/<usuario>/<slug:quiz_slug>/<int:pregunta_id>/<int:answer_id>/', views.deleteAnswers, name='delete_answers'),
     path('quiz/eliminar/<usuario>/<int:quiz_id>/', views.deleteQuiz, name='delete_quiz'),
-    path('quiz/editar-opcion/<usuario>/<slug:quiz_slug>/<int:pregunta_id>/<int:answer_id>/', views.editAnswer, name='edit_answer'),
-    path('quiz/editar-quiz/<usuario>/<slug:quiz_slug>/', views.editQuiz, name='edit_quiz'),
-    
+    path('quiz/editar-opcion/<usuario>/<slug:quiz_slug>/<int:pregunta_id>/<int:answer_id>/', views.editAnswer, name='edit_answer'),    
 
     
 ]
